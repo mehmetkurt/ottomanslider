d1e9221f833f82ac2fe3da6d41f979c1114bd59f
mehmetkurt <info@mehmetkurt.com.tr>
2014-10-25 03:29:36 +0300

    Add to Files on windows
    Add to development files

---
+++ js/data.json
@@ -0,0 +1,167 @@
+﻿var data = [
+   {
+       "menu": {
+           "id": "1",
+           "title": "FANTASIC MENU",
+           "item": [
+             {
+                 "text": "Lorem İpsum",
+                 "url": "#",
+                 "title": "1"
+             },
+              {
+                  "text": "Lorem İpsum",
+                  "url": "#",
+                  "title": "2"
+              },
+              {
+                  "text": "Lorem İpsum",
+                  "url": "#",
+                  "title": "3"
+              },
+              {
+                  "text": "Lorem İpsum",
+                  "url": "#",
+                  "title": "4"
+              },
+              {
+                  "text": "Lorem İpsum",
+                  "url": "#",
+                  "title": "5"
+              }
+           ]
+       }
+   },
+    {
+        "menu": {
+            "id": "2",
+            "title": "FANTASIC MENU",
+            "item": [
+                {
+                    "text": "Lorem İpsum",
+                    "url": "#",
+                    "title": "1"
+                },
+                {
+                    "text": "Lorem İpsum",
+                    "url": "#",
+                    "title": "2"
+                },
+                {
+                    "text": "Lorem İpsum",
+                    "url": "#",
+                    "title": "3"
+                },
+                {
+                    "text": "Lorem İpsum",
+                    "url": "#",
+                    "title": "4"
+                },
+                {
+                    "text": "Lorem İpsum",
+                    "url": "#",
+                    "title": "5"
+                }
+            ]
+        }
+    },
+    {
+        "menu": {
+            "id": "3",
+            "title": "FANTASIC MENU",
+            "item": [
+                {
+                    "text": "Lorem İpsum",
+                    "url": "#",
+                    "title": "1"
+                },
+                {
+                    "text": "Lorem İpsum",
+                    "url": "#",
+                    "title": "2"
+                },
+                {
+                    "text": "Lorem İpsum",
+                    "url": "#",
+                    "title": "3"
+                },
+                {
+                    "text": "Lorem İpsum",
+                    "url": "#",
+                    "title": "4"
+                },
+                {
+                    "text": "Lorem İpsum",
+                    "url": "#",
+                    "title": "5"
+                }
+            ]
+        }
+    },
+    {
+        "menu": {
+            "id": "4",
+            "title": "FANTASIC MENU",
+            "item": [
+                {
+                    "text": "Lorem İpsum",
+                    "url": "#",
+                    "title": "1"
+                },
+                {
+                    "text": "Lorem İpsum",
+                    "url": "#",
+                    "title": "2"
+                },
+                {
+                    "text": "Lorem İpsum",
+                    "url": "#",
+                    "title": "3"
+                },
+                {
+                    "text": "Lorem İpsum",
+                    "url": "#",
+                    "title": "4"
+                },
+                {
+                    "text": "Lorem İpsum",
+                    "url": "#",
+                    "title": "5"
+                }
+            ]
+        }
+    },
+       {
+           "menu": {
+               "id": "5",
+               "title": "FANTASIC MENU",
+               "item": [
+                 {
+                     "text": "Lorem İpsum",
+                     "url": "#",
+                     "title": "1"
+                 },
+                  {
+                      "text": "Lorem İpsum",
+                      "url": "#",
+                      "title": "2"
+                  },
+                  {
+                      "text": "Lorem İpsum",
+                      "url": "#",
+                      "title": "3"
+                  },
+                  {
+                      "text": "Lorem İpsum",
+                      "url": "#",
+                      "title": "4"
+                  },
+                  {
+                      "text": "Lorem İpsum",
+                      "url": "#",
+                      "title": "5"
+                  }
+               ]
+           }
+       }
+];
---
+++ js/jquery.memo.js
@@ -0,0 +1,141 @@
+﻿/* MEHMET KURT - 2013 - Telif Hakkı falan yok :) 
+ * info@mehmetkurt.com.tr
+ * http://www.mehmetkurt.com.tr
+ * 
+ */
+$(document).ready(function () {
+    //alert('a');
+    $('#memosliderv1').mehmetkurt({
+        padding: 10,
+        copyTitle: true
+    });
+
+});
+
+/* */
+(function ($) {
+    $.fn.mehmetkurt = function (options) {
+
+        var defaults = {
+            caret: 'bigcaret',
+            animate: true,
+            padLeft: 10,
+            padRight: 10,
+            padTop: 10,
+            padBottom: 10,
+            padding: 0,
+            copyTitle: false
+        };
+
+        var settings = $.extend(defaults, options);
+
+        var mainImage = this.find('.main-image');
+        var _height = mainImage.height();
+
+        var oldBg = '';
+        this.find('.thumb').hover(
+                 function () {
+                     var bg = $(this).attr('data-bg');
+                     oldBg = mainImage.css('background-image');
+                     mainImage.css('background-image', 'url(' + bg + ')');
+                 },
+                 function () {
+                     mainImage.css('background-image', 'url(' + oldBg + ')');
+                 }
+             );
+
+        this.find('.thumb').each(function () {
+            var id = $(this).attr('data-id');
+            var bg = $(this).attr('data-bg');
+            var thumb = $(this);
+
+            var menuString = '<div class="menu"><h5>Başlık</h5></div>';
+            thumb.append(menuString);
+
+            $.each(data, function (key, value) {
+                var menu = value.menu;
+                if (menu.id == id) {
+                    var innerMenu = thumb.find('.menu');
+                    innerMenu.find('h5').html(menu.title);
+
+                    if (menu.item.length > 0)
+                        innerMenu.append('<ul></ul>');
+
+                    $.each(menu.item, function (k, item) {
+                        var text = item.text;
+                        var url = item.url;
+                        var title = item.title;
+
+                        innerMenu.find('ul').append(
+                            $('<li/>', {
+                                html: $('<a/>', {
+                                    href: url,
+                                    text: text,
+                                    title: title
+                                })
+                            })
+                        );
+                    });
+                }
+            });
+
+            /* DOSYADAN OKUYOR. LOCALDE ÇALIŞMAZ. IIS üzerinde yayınlamak için .json mime type tanımlanmalı. Eğer aşağıdaki satır açılırsa bir üstteki each kapatılmalı. bunuda bir ara otomatiğe bağlıcaz inşallah. */
+            //var uri = "../slidermenu.json";
+            //$.ajax({
+            //    url: uri,
+            //    async: true,
+            //    contentType: "application/json; charset=utf-8",
+            //    dataType: "json",
+            //    cache: false,
+            //    success: function (data) {
+            //        $.each(data, function (key, value) {
+            //            var menu = value.menu;
+            //            if (menu.id == id) {
+            //                var innerMenu = thumb.find('.menu');
+            //                innerMenu.find('h5').html(menu.title);
+
+            //                if (menu.item.length > 0)
+            //                    innerMenu.append('<ul></ul>');
+
+            //                $.each(menu.item, function (k, item) {
+            //                    var text = item.text;
+            //                    var url = item.url;
+            //                    var title = item.title;
+
+            //                    innerMenu.find('ul').append(
+            //                        $('<li/>', {
+            //                            html: $('<a/>', {
+            //                                href: url,
+            //                                text: text,
+            //                                title: title
+            //                            })
+            //                        })
+            //                    );
+            //                });
+            //            }
+            //        });
+            //    },
+            //    error: function (xhr, ajaxOptions, thrownError) {
+            //        console.log(thrownError);
+            //    }
+            //});
+
+            var padLR = settings.padLeft + settings.padRight;
+            var padTB = settings.padTop + settings.padBottom;
+
+            if (settings.padding > 0) {
+                padLR = settings.padding * 2;
+                padTB = settings.padding * 2;
+            }
+
+            var _width = $(this).width() - padLR;
+            $(this).find('.menu').width(_width).height(_height - padTB);
+
+            if (settings.copyTitle) {
+                $(this).find('.menu h5').html($(this).find('.button').html());
+            }
+
+            return this;
+        });
+    }
+}(jQuery));
